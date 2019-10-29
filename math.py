f6d66358ed6e8ca914260b5d2d34096782988d4d
Ubuntu <ubuntu@ip-172-31-28-26.ec2.internal>
2019-10-29 18:26:26 +0000

    Initial commit

---
+++ math.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Evano Fotia
+# Created on: oct 2019
+# This program does math
+
+
+def main():
+    print("7-3={}".format(7-3))
+    print("4/2={}".format(4/2))
+    print("7+3={}".format(7+3))
+
+
+if __name__ == "__main__":
+    main()
